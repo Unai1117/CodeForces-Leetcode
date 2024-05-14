a805aaef2077093d2ec9e714857e1fac0ef44865
Unai López <unailopez1117@gmail.com>
2024-05-14 14:37:22 +0200

    Valid Anagram

--- anagram.java
+++ anagram.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.util.HashMap;
 
-public class problems {
+public class anagram {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		while (sc.hasNext()) {
