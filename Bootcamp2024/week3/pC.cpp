0a74abffb08a1a54abaa0bcf0ec4f4720718acc2
Unai1117 <unailopez1117@gmail.com>
2024-07-29 09:45:46 -0200

    week 3

--- Bootcamp2024/week3/pC.cpp
+++ Bootcamp2024/week3/pC.cpp
@@ -8,12 +8,39 @@ typedef unsigned long long ull;
 #define REP(i, a, b) for (int i = a; i < b; ++i)
 #define REPV(i, a, b) for (int i = a; i >= b; --i)
 #define MOD 1000000007
+const double pi = 3.141592; 
 
 void solve() {
-	
+	int F; 
+	cin >> F;
+	if(F == 1){
+		double r; 
+		cin >> r;
+		double area = pi * r * r;
+		cout << fixed << setprecision(5) << area; ent;
+	} else if(F == 2){
+		double x;
+		cin >> x;
+		double area = x * x; 
+		cout << fixed << setprecision(5) << area; ent;
+	} else if(F == 3){
+		double x, y; 
+		cin >> x >> y;
+		double area = x * y; 
+		cout << fixed << setprecision(5) << area; ent;
+	} else {
+		double b, h;
+		cin >> b >> h; 
+		double area = (b * h) / 2;
+		cout << fixed << setprecision(5) << area; ent;
+	}
 }
 
 int main() {
-	solve(); 
+	int n; 
+	cin >> n;
+	while(n--){
+		solve();
+	}
 	return 0; 
 }
