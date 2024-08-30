d8e87f905a9ef41874e7c0d56e498fb42ed3b5d8
Unai1117 <unailopez1117@gmail.com>
2024-08-30 13:18:44 +0200

    F mal

--- Bootcamp2024/week7/pF.cpp
+++ Bootcamp2024/week7/pF.cpp
@@ -12,7 +12,46 @@ int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
 void solve() {
-	
+	int q;
+    cin >> q;
+    
+    unordered_map<ll, ll> tareas;
+    priority_queue<pair<ll, ll>> maxHeap; 
+    
+    while (q--) {
+        char type;
+        cin >> type;
+        
+        if (type == '+') {
+            ll pr, id;
+            cin >> pr >> id;
+            tareas[id] = pr;
+            maxHeap.push({pr, id});
+        } 
+        else if (type == '-') {
+            ll id;
+            cin >> id;
+            if (tareas.find(id) != tareas.end()) {
+                cout << id << endl;
+                tareas.erase(id);
+            }
+        } 
+        else if (type == '!') {
+            while (!maxHeap.empty()) {
+                ll pr = maxHeap.top().first;
+                ll id = maxHeap.top().second;
+                
+                if (tareas.find(id) != tareas.end() && tareas[id] == pr) {
+                    cout << id << endl;
+                    tareas.erase(id); 
+                    maxHeap.pop(); 
+                    break;
+                } else {
+                    maxHeap.pop();
+                }
+            }
+        }
+    }
 }
 
 int main() {
