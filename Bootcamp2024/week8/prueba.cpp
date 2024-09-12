bfdc9a37110b7b80f7e28d80baad2f9a25eecabe
Unai1117 <unailopez1117@gmail.com>
2024-09-12 19:23:21 +0200

    .

--- Bootcamp2024/week8/prueba.cpp
+++ Bootcamp2024/week8/prueba.cpp
@@ -1,20 +1,14 @@
-#include <bits/stdc++.h>
-using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
-#define entf cout << endl
-#define ent cout << "\n"
-#define REPN(n) for(int i = 0; i < n; ++i)
-#define REP(i, a, b) for (int i = a; i < b; ++i)
-#define REPV(i, a, b) for (int i = a; i >= b; --i)
-#define MOD 1000000007
-int dx[] = {-1, 1, 0, 0};
-int dy[] = {0, 0, -1, 1};
+#include <iostream>
+#include <vector>
 
-void completar_grafo(int n, vector<pair<int, int>>& edges) {
+using namespace std;
+
+// Función para completar el grafo
+void completar_grafo(int n, int m, vector<pair<int, int>>& edges) {
     // Crear y rellenar la matriz de adyacencia
     vector<vector<bool>> adj(n + 1, vector<bool>(n + 1, false));
 
+    // Marcar las aristas existentes
     for (auto& edge : edges) {
         int u = edge.first;
         int v = edge.second;
@@ -22,6 +16,7 @@ void completar_grafo(int n, vector<pair<int, int>>& edges) {
         adj[v][u] = true;
     }
 
+    // Encontrar las aristas faltantes
     vector<pair<int, int>> missing_edges;
     for (int i = 1; i <= n; ++i) {
         for (int j = i + 1; j <= n; ++j) {
@@ -31,22 +26,27 @@ void completar_grafo(int n, vector<pair<int, int>>& edges) {
         }
     }
 
+    // Imprimir el número de aristas que se necesitan
     cout << missing_edges.size() << endl;
 
+    // Imprimir las aristas faltantes
     for (auto& edge : missing_edges) {
         cout << edge.first << " " << edge.second << endl;
     }
 }
 
 int main() {
+    // Leer la entrada
     int n, m;
     cin >> n >> m;
     vector<pair<int, int>> edges(m);
-
+    
     for (int i = 0; i < m; ++i) {
         cin >> edges[i].first >> edges[i].second;
     }
 
-    completar_grafo(n, edges);
+    // Llamar a la función para completar el grafo
+    completar_grafo(n, m, edges);
+
     return 0;
 }
