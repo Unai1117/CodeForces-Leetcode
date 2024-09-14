1d3211cbf6993ad85b1da69b0cdf1fca9e2ce5ad
Unai1117 <unailopez1117@gmail.com>
2024-09-14 11:36:58 +0200

    .

--- Bootcamp2024/week8/pF.cpp
+++ Bootcamp2024/week8/pF.cpp
@@ -1,4 +1,7 @@
 #include <bits/stdc++.h>
+#include <functional>
+#include <limits>
+#include <queue>
 using namespace std;
 typedef long long ll;
 typedef unsigned long long ull;
@@ -11,11 +14,64 @@ typedef unsigned long long ull;
 int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
-void solve() {
-	
+struct Edge {
+    int to, length;
+};
+
+struct State {
+    int node;
+    double time;
+    bool operator>(const State& other) const {
+        return time > other.time;
+    }
+};
+
+vector<double> dijkstra(int start, int n, const vector<vector<Edge>>& adj, double speed) {
+    vector<double> dist(n+1, numeric_limits<double>::infinity());
+    priority_queue<State, vector<State>, greater<State>> pq;
+    dist[start] = 0.0;
+    pq.push({start, 0.0});
+
+    while(!pq.empty()) {
+        State current = pq.top();
+        pq.pop();
+
+        if(current.time > dist[current.node]) continue;
+        for(const Edge& edge : adj[current.node]) {
+            double new_time = current.time + edge.length / speed;
+            if(new_time < dist[edge.to]) {
+                dist[edge.to] = new_time;
+                pq.push({edge.to, new_time});
+            }
+        }
+    }
+    return dist;
 }
 
 int main() {
-	solve(); 
-	return 0; 
+	int n, t, va, vb, a, b;
+	cin >> n >> t >> va >> vb >> a >> b;
+	vector<vector<Edge>> adj(n+1);
+	for(int i = 0; i < n-1; ++i) {
+	   int u, v, d;
+		cin >> u >> v >> d;
+		adj[u].push_back({v, d});
+		adj[v].push_back({u, d});
+	}
+
+	vector<double> dist_a = dijkstra(a, n, adj, va);
+	vector<double> dist_b = dijkstra(b, n, adj, vb);
+	vector<int> meeting_points;
+	for(int i = 1; i <= n; ++i) {
+	   if(dist_a[i] <= t && dist_b[i] <= t){
+			meeting_points.push_back(i);
+		}
+	}
+
+	cout << meeting_points.size() << endl;
+    for (int place : meeting_points) {
+        cout << place << " ";
+    }
+    cout << endl;
+	return 0;
 }
