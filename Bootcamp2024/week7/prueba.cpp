e21b6fdca7f46e7e15736504993a70bc4b851e06
Unai1117 <unailopez1117@gmail.com>
2024-09-16 18:15:15 +0200

    .

--- Bootcamp2024/week7/prueba.cpp
+++ Bootcamp2024/week7/prueba.cpp
@@ -1,22 +1,23 @@
-#include <iostream>
-#include <unordered_map>
-#include <queue>
+#include <bits/stdc++.h>
 using namespace std;
-
 typedef long long ll;
+typedef unsigned long long ull;
+#define entf cout << endl
+#define ent cout << "\n"
+#define REPN(n) for(int i = 0; i < n; ++i)
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define REPV(i, a, b) for (int i = a; i >= b; --i)
+#define MOD 1000000007
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
 
-struct Compare {
-    bool operator()(const pair<ll, ll>& a, const pair<ll, ll>& b) {
-        return a.first < b.first; // Max-heap based on priority
-    }
-};
-
-int main() {
-    int q;
+void solve() {
+	int q;
     cin >> q;
     
     unordered_map<ll, ll> tareas;
-    priority_queue<pair<ll, ll>, vector<pair<ll, ll>>, Compare> maxHeap; 
+    priority_queue<pair<ll, ll>> maxHeap; 
+    unordered_set<ll> eliminados;
     
     while (q--) {
         char type;
@@ -34,24 +35,27 @@ int main() {
             if (tareas.find(id) != tareas.end()) {
                 cout << id << endl;
                 tareas.erase(id);
+                eliminados.insert(id);
             }
         } 
         else if (type == '!') {
             while (!maxHeap.empty()) {
                 ll pr = maxHeap.top().first;
                 ll id = maxHeap.top().second;
-                
                 if (tareas.find(id) != tareas.end() && tareas[id] == pr) {
                     cout << id << endl;
-                    tareas.erase(id); 
-                    maxHeap.pop(); 
+                    tareas.erase(id);
+                    maxHeap.pop();
                     break;
-                } else {
-                    maxHeap.pop(); // Remove invalid elements
+                } else if (eliminados.find(id) != eliminados.end()) {
+                    maxHeap.pop();  
                 }
             }
         }
     }
-    
-    return 0;
+}
+
+int main() {
+	solve(); 
+	return 0; 
 }
