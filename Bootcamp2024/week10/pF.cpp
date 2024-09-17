732c43f75599667e452d12ceccdddadee06c5108
Unai1117 <unailopez1117@gmail.com>
2024-09-17 12:41:22 +0200

    .

--- Bootcamp2024/week10/pF.cpp
+++ Bootcamp2024/week10/pF.cpp
@@ -12,7 +12,23 @@ int dx[] = {-1, 1, 0, 0};
 int dy[] = {0, 0, -1, 1};
 
 void solve() {
-	
+	int n, m;
+	cin >> n >> m;
+	int arr[n][m];
+	for(int i = 0; i < n; ++i){
+		for(int j = 0; j < m; ++j){
+			cin >> arr[i][j];
+			if(i == 0 && j == 0) continue;
+			if(i == 0){
+				arr[i][j] += arr[i][j-1];
+			} else if(j == 0){
+				arr[i][j] += arr[i-1][j];
+			} else {
+				arr[i][j] += max(arr[i-1][j], arr[i][j-1]);
+			}
+		}
+	}
+	cout << arr[n-1][m-1] << endl;
 }
 
 int main() {
