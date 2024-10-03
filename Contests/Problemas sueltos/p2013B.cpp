d5e4cdb6ab4cd5a62760686f899846f826b0792b
unai1117 <unailopez1117@gmail.com>
2024-10-03 12:26:02 +0200

    .

---
+++ Contests/Problemas sueltos/p2013B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define entf cout << endl
+#define ent cout << "\n"
+#define REPN(n) for(int i = 0; i < n; ++i)
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define REPV(i, a, b) for (int i = a; i >= b; --i)
+#define MOD 1000000007
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+void solve() {
+	int n; cin >> n; 
+    ll ans = 0; 
+    vector<int> a(n);
+    REPN(n) {
+        cin >> a[i];
+        ans += a[i];
+    }
+    cout << ans - 2*a[n-2] << endl;
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--){
+	    solve(); 
+    }
+	return 0; 
+}
