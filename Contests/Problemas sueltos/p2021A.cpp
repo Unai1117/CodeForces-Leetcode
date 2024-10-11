d3f8745314ac3d79c5fa62b12ec36dd127bc444f
unai1117 <unailopez1117@gmail.com>
2024-10-11 09:30:34 +0200

    More Div 2 level problems

---
+++ Contests/Problemas sueltos/p2021A.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define entf cout << endl
+#define ent cout << "\n"
+#define REPN(n) for(int i = 0; i < n; ++i)
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define REPV(i, a, b) for (int i = a; i >= b; --i)
+#define MOD 1000000007
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+void solve() {
+	int n; cin >> n;
+	vector<int> a(n);
+	REPN(n) cin >> a[i];
+	sort(a.begin(), a.end());
+	int ans = (a[0] + a[1])/2;
+	for(int i = 2; i < n; ++i) {
+	   ans = (ans+a[i])/2;
+	}
+	cout << ans; entf;
+}
+
+int main() {
+    int t; cin >> t;
+    while(t--){
+	    solve();
+    }
+	return 0;
+}
