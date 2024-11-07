f9cd18d42198b0a70367bdf18877aa434749c13f
unai1117 <unailopez1117@gmail.com>
2024-11-07 12:44:43 +0100

    .

---
+++ Contests/R984/pA.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+#define entf cout << endl
+#define ent cout << "\n"
+#define REPN(n) for(int i = 0; i < n; ++i)
+#define REP(i, a, b) for (int i = a; i < b; ++i)
+#define REPV(i, a, b) for (int i = a; i >= b; --i)
+#define MOD 1000000007
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+void solve() {
+	int n; cin >> n;
+	vector<int> a(n);
+	REPN(n) {
+		cin >> a[i];
+	}
+	REPN(n-1) {
+		int diff = abs(a[i] - a[i + 1]);
+		if(diff != 5 && diff != 7){
+			cout << "NO";
+			ent;
+			return;
+		}
+	}
+	cout << "YES" << endl;
+}
+
+int main() {
+	int t; cin >> t;
+	while(t--) {
+		solve();
+	}
+	return 0; 
+}
